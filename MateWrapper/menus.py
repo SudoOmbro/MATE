674fb44777bc71b7d326ea0136cf768132682d06
mferrari <mferrari@mailup.com>
2022-03-10 17:28:45 +0100

    started implementing menu concept

--- MateWrapper/menus.py
+++ MateWrapper/menus.py
@@ -1,10 +1,21 @@
 from typing import Dict, List
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Handler, CallbackQueryHandler, ConversationHandler
 
 from MateWrapper.generics import TelegramFunctionBlueprint
 from MateWrapper.prompts import Prompt
 from MateWrapper.generics import Chain
+
+
+__BUTTON_COUNTER: int = 0
+
+
+def _get_button_handle() -> str:
+    global __BUTTON_COUNTER
+    current_id = __BUTTON_COUNTER
+    __BUTTON_COUNTER += 1
+    return f"__b{current_id}"
 
 
 def get_menu_handler(
@@ -41,6 +52,80 @@ def get_menu_handler(
     return handlers
 
 
-def generate_conversation(entry_points: List[Handler], menu: dict, fallback: List[Handler]) -> ConversationHandler:
-    # TODO
-    pass
+class Button:
+
+    def __init__(self, text: str):
+        if not text:
+            raise ValueError("A button needs some text in it")
+        self.text: str = text
+        self.handle: str = _get_button_handle()
+
+    def setup(self, parent_menu: "Menu", parent_submenu: "Panel"):
+        # implement if the button needs the menu's context to initialize itself
+        pass
+
+    def callback_handler(self) -> callable or None:
+        return None
+
+    def keyboard_button(self) -> InlineKeyboardButton:
+        raise NotImplemented()
+
+
+class UrlButton(Button):
+
+    def __init__(self, text: str, url: str):
+        super().__init__(text)
+        if not url:
+            raise ValueError("The url cannot be empty")
+        self.url = url
+
+    def keyboard_button(self) -> InlineKeyboardButton:
+        return InlineKeyboardButton(self.text, url=self.url)
+
+
+class FuncButton(Button):
+
+    def __init__(self, text: str, function: callable):
+        super().__init__(text)
+        if not function:
+            raise ValueError("The function cannot be None")
+        self.func = function
+
+    def keyboard_button(self) -> InlineKeyboardButton:
+        return InlineKeyboardButton(self.text, callback_data=self.handle)
+
+    def callback_handler(self) -> callable or None:
+        return self.func
+
+
+class Panel:
+
+    def __init__(self, prompt: Prompt, buttons: List[Button or List[Button]], back_to: str):
+        self.prompt = prompt
+        self.buttons = buttons
+        self.back_to = back_to
+
+    def keyboard(self) -> InlineKeyboardMarkup:
+        keyboard_list: List[List[InlineKeyboardButton]] = []
+        # TODO
+        return InlineKeyboardMarkup(keyboard_list)
+
+
+class Menu:
+
+    def __init__(
+            self,
+            entry_points: List[Handler],
+            submenus: Dict[str, Panel],
+            fallbacks: List[Handler] or None = None
+    ):
+        self.entry_points = entry_points,
+        self.submenus = submenus
+        self.fallbacks = fallbacks
+
+    def get_conversation(self) -> ConversationHandler:
+        return ConversationHandler(
+            entry_points=self.entry_points,
+            states={},  # TODO
+            fallbacks=self.fallbacks
+        )
